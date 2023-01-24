59e40d082e0ff51ffacc551d3d11793dfc70f8bf
aleksejsss <aleksejssematjuks@gmail.com>
2023-01-24 23:43:34 +0200

    added data from database

--- Kivy/main.py
+++ Kivy/main.py
@@ -2,31 +2,32 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
 from kivy.properties import StringProperty
+import sqlite3 as db
 
-klases =['a','b','c','d']
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,disable_multitouch') #multitouch atslēgšana
 
-class WidgetEx(GridLayout):
-    my_text = StringProperty("Button has not been clicked")
-    x = 0
-    def buttonPress(self,):
-        self.x = self.x + 1
-        print('Button has been clicked')
-        self.my_text = "Button has been clicked {} times".format(self.x)
-
+with db.connect('datubaze.db') as con:
+    cur = con.execute("""SELECT klase, vards_uzvards, skolena_id FROM skolenu_saraksts
+    """)
+    skoleni = cur.fetchall()
+    
 class List(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        for i in range(0,12):
-            for j in klases:
-                self.add_widget(Button(
-                    text='{}.{}'.format(i+1,j),
+        for i in skoleni:
+            self.add_widget(Button(
+                    text=f'{i[0]} - {i[1]}',
+                    text_size= (200,None),    
+                    halign= 'left',
+                    valign= 'bottom',
                     size_hint=(1,None),
                     size=(dp(20),dp(20))
                     ))
-    
                 
 class BoxlayoutEx(BoxLayout):
     pass
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sqlite3 as db
 with db.connect('datubaze.db') as con:
-    cur = con.execute("""SELECT klase FROM skolenu_saraksts WHERE id = 1
+    cur = con.execute("""SELECT klase, vards_uzvards FROM skolenu_saraksts
     """)
     skoleni = cur.fetchall()
-for i in skoleni:
-    print(i[2],' ar personas kodu ',i[3])
+    for i in skoleni:print(' '.join(i))
     
