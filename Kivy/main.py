b26481fb86779f9f6e9ac2d0137024a8ed0a7762
aleksejsss <79640617+aleksejsss@users.noreply.github.com>
2023-02-15 11:25:32 +0200

    Atlases sākums

--- Kivy/main.py
+++ Kivy/main.py
@@ -7,27 +7,38 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
 from kivy.properties import StringProperty
 import sqlite3 as db
-
+import kivy
 from kivy.config import Config
+
+from functools import partial
+
 Config.set('input', 'mouse', 'mouse,disable_multitouch') #multitouch atslēgšana
+kivy.require("2.0.0")
 
 with db.connect('datubaze.db') as con:
-    cur = con.execute("""SELECT klase, vards_uzvards, skolena_id FROM skolenu_saraksts
+    cur = con.execute("""SELECT * FROM skolenu_saraksts
     """)
     skoleni = cur.fetchall()
-    
+    #izveletais_skolens = skoleni[0][0]
+    #print(izveletais_skolens)
+
 class List(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         for i in skoleni:
-            self.add_widget(Button(
-                    text=f'{i[0]} - {i[1]}',
-                    text_size= (200,None),    
-                    halign= 'left',
-                    valign= 'bottom',
-                    size_hint=(1,None),
-                    size=(dp(20),dp(20))
-                    ))
+            if i[9] == 1:
+                self.add_widget(Button(
+                        text=f'{i[1]} - {i[2]}',
+                        text_size= (200,None),    
+                        halign= 'left',
+                        valign= 'bottom',
+                        size_hint=(1,None),
+                        size=(dp(20),dp(20)),
+                        on_press= partial(set_variables,i[0])
+                        ))
+                
+def set_variables(skolena_id,a):
+    print(skolena_id)
                 
 class BoxlayoutEx(BoxLayout):
     pass
